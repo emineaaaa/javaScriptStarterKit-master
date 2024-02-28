fa035214b31959bca05055049e43997dfd174ca1
emineaaaa <emine0aydinli3@gmail.com>
2024-02-28 17:59:40 +0300

    updates

---
+++ src/app.js
@@ -0,0 +1,25 @@
+console.log("hadi bismillahh")
+
+let dolarDun=9.20
+let dolarBugun=9.30
+
+{
+    let dolarDun=9.10
+}
+
+const euroDun=11    //const ile tanımlanan değişken bir daha değiştirilemez  
+
+console.log(dolarDun)
+
+//array tanımlama (camelCasing ile)
+let konutKredileri=["ev k","emlak k","taşıt k","özel k"]
+
+console.log("<ul>")
+for(let i=0;i<konutKredileri.length;i++){
+    console.log("<li>"+konutKredileri[i]+"</li>")
+}
+console.log("</ul>")
+
+
+
+console.log(konutKredileri)
